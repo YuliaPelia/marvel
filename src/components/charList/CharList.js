c89b5b14b6f0082bb39878efdde6170936427b41
Yulia <yuliapelia18@gmail.com>
2023-03-05 12:34:40 +0200

    changed components

---
+++ src/components/charList/CharList.js
@@ -0,0 +1,117 @@
+import { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import useMarvelService from '../../service/MarvelService';
+import './charList.scss';
+
+const CharList = (props) => {
+
+    const [chars, setChars] = useState([]);
+    const [offset, setOffset] = useState(44);
+    const [charEnded, setCharsEnded] = useState(false);
+    const [loandingMore, setLoadingMore] = useState(false);
+
+    const {loading, error, getAllCharacters} = useMarvelService();
+
+
+
+    useEffect(() => {
+        onRequest(offset, true);
+    }, [])
+
+    const onRequest = (offset, initial) => {
+        initial ? setLoadingMore(false) : setLoadingMore(true)
+        getAllCharacters(offset)
+            .then(onCharAllLoaded)
+    }
+
+    const onCharAllLoaded = (newChars) => { 
+        let ended = false;
+        if(newChars.length < 9) {
+            ended = true
+        }
+
+        setChars(chars => [...chars, ...newChars]);
+        setLoadingMore(LoadingMore => false)
+        setOffset(offset => offset + 9);
+        setCharsEnded(charEnded => ended);
+
+    }
+
+
+    const itemRefs = useRef([]);
+
+
+    const focusOnItem = (id) => {
+        itemRefs.current.forEach(item => item.classList.remove('char__item_selected'));
+        itemRefs.current[id].classList.add('char__item_selected');
+        itemRefs.current[id].focus();
+    }
+
+    function ViewItem (arr) {
+
+        const items = arr.map((char, i) => {
+            let imgStyle = {'objectFit' : 'cover'};
+            if (char.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+                imgStyle = {'objectFit' : 'unset'};
+            }
+            return (
+                <li 
+                    className="char__item"
+                    tabIndex={0}
+                    ref={el => itemRefs.current[i] = el}
+                    key={char.id}
+                    onClick={() => {
+                        props.onCharSelected(char.id);
+                        focusOnItem(i);
+                    }}
+                    onKeyDown={(e) => {
+                        if (e.key === ' ' || e.key === "Enter") {
+                            props.onCharSelected(char.id);
+                            focusOnItem(i);
+                        }
+                    }}>
+                        <img src={char.thumbnail} alt={char.name} style={imgStyle}/>
+                        <div className="char__name">{char.name}</div>
+                </li>
+            )
+        }); 
+
+        return (
+            <ul className="char__grid">
+                {items}
+            </ul>
+        )
+    }
+
+    
+
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading && !loandingMore ? <Spinner/> : null;
+    const content = ViewItem(chars);
+
+    return (
+        <div className="char__list">
+                {errorMessage}
+                {spinner}
+                {content}
+
+                <button 
+                className="button button__main button__long"
+                disabled={loandingMore}
+                style={{'display': charEnded ? 'none' : 'block'}}
+                onClick={() => onRequest(offset)}>
+                    <div className="inner">Load more</div>
+                </button>
+        </div>
+    )
+
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
+}
+
+export default CharList;
---
+++ src/service/MarvelService.js
@@ -0,0 +1,44 @@
+import { useHttp } from "../hooks/http.hook";
+
+const useMarvelService = () => {
+    const {loading, request, error, clearError} = useHttp();
+
+    const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    const _apiKey = 'apikey=2813600ead6a7195f6ec5f57400c79d3';
+    const _apiNum = 80;
+
+
+    // будемо робити запити до нашого сервера(api)
+    // запит по всіх персонажах
+    const getAllCharacters = async (offset = _apiNum) => {
+        const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformCharacter);
+        // res.data.results.map(); - для того щоб сформувати масив з новими обєктами
+        // ?? - якщо перша змінна undefined || null тоді буде те що з права
+    }
+
+
+    // запит по певному одному персонажу
+
+    const getCharacter = async (id) => {
+        const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+        return _transformCharacter(res.data.results[0]);
+    }
+
+    const _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? char.description.slice(0, 210) : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url, 
+            wiki: char.urls[1].url,
+            comics: char.comics.items
+            
+        }
+    }
+    
+    return {loading, error, getAllCharacters, getCharacter, clearError}
+}
+
+export default useMarvelService;
